d9324c2f3cf4bf3a7a137c9a71ac9c05d513de0b
John Gardner <john@arconixpc.com>
2014-07-30 21:16:14 -0400

    fix version number

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
  * Plugin URI: http://arconixpc.com/plugins/arconix-faq
  * Description: Plugin to handle the display of FAQs
  *
- * Version: 1.4.2
+ * Version: 1.4.3
  *
  * Author: John Gardner
  * Author URI: http://arconixpc.com/
