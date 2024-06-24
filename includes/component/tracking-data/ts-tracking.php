22d4e19e2bd5f82cf3ce28a3f73a9742d1915fc3
Nidhi <nidhi@tychesoftwares.com>
2024-06-24 12:47:22 +0530

    Fix #125
    Fixed vulnerable to Broken Access Control added nonce check.
    Fix #125

--- includes/component/tracking-data/ts-tracking.php
+++ includes/component/tracking-data/ts-tracking.php
@@ -131,10 +131,11 @@ class FAQ_TS_tracking {
 	public static function ts_plugin_action_links ( $links ) {
 		
 		$ts_action_links = array();
+		$nonce           = wp_create_nonce( 'ts_nonce_action' );
 		
 		if ( 'unknown' != get_option( self::$plugin_prefix . '_allow_tracking', 'unknown' ) ) {
 			
-			$ts_action = self::$ts_settings_page . "&ts_action=reset_tracking"; 
+			$ts_action = self::$ts_settings_page . '&ts_action=reset_tracking&nonce=' . $nonce;
 			
 			$ts_action_links = array(
 				'reset_tracking' => '<a href="'.$ts_action.'" class="reset_tracking" title= "This will reset your usage tracking settings, causing it to show the opt-in banner again and not sending any data.">Reset Usage Tracking</a>',
@@ -168,12 +169,15 @@ class FAQ_TS_tracking {
 	 * It will delete the tracking option from the database.
 	 */
 	public static function ts_reset_tracking_setting () {
+		$nonce = isset( $_GET ['nonce'] ) ? $_GET['nonce'] : '';//phpcs:ignore
+		if ( is_user_logged_in() && current_user_can( 'manage_options' ) && wp_verify_nonce( $nonce, 'ts_nonce_action' ) ) {
 
-		if ( isset ( $_GET [ 'ts_action' ] ) && 'reset_tracking' == $_GET [ 'ts_action' ] ) {
-			delete_option( self::$plugin_prefix . '_allow_tracking' );
-			delete_option( self::$plugin_prefix . '_ts_tracker_last_send' );
-			$ts_url = remove_query_arg( 'ts_action' );
-			wp_safe_redirect( $ts_url );
+			if ( isset ( $_GET [ 'ts_action' ] ) && 'reset_tracking' == $_GET [ 'ts_action' ] ) {
+				delete_option( self::$plugin_prefix . '_allow_tracking' );
+				delete_option( self::$plugin_prefix . '_ts_tracker_last_send' );
+				$ts_url = remove_query_arg( 'ts_action' );
+				wp_safe_redirect( $ts_url );
+			}
 		}
 	}
 
